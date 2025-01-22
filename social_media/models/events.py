5d2f74823e617f9357a0f52e6a574b30012787d2
Dilara Mukasheva (k23008889) <mukashevadilara41@gmail.com>
2025-01-22 17:51:05 +0000

    Model
    Modify Event model

--- social_media/models/events.py
+++ social_media/models/events.py
@@ -7,7 +7,7 @@ class Events(models.Model):
     """Model used for events in the societies"""
 
     name = models.CharField(max_length=250, blank=False)
-    society_id = models.ForeignKey('Society', on_delete=models.CASCADE)
+    society_id = models.ForeignKey(Society, on_delete=models.CASCADE)
     description = models.CharField(max_length=1000, blank=False)
     date = models.DateField(blank=False) 
     location = models.CharField(max_length=250, blank=False)
