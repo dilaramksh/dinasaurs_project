fb935f4a0bbc51ffd60a81e916eb63b654818ada
Maryam Marei <maryam.marei@kcl.ac.uk>
2025-01-30 11:26:23 +0000

    create decorator for user type and unathorized html

--- social_media/student_views.py
+++ social_media/student_views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from social_media.decorators import user_type_required
+
 
 #to do: add login required
 #to do: add user type required
+
+#@user_type_required('student')
+#@login_required
 def student_dashboard(request):
     return render(request, 'student/student_dashboard.html')
+
+#to be added once login is configured
+#def student_dashboard(request):
+ #   context = {
+  #      'user':'student',
+   # }
+    #return render(request, 'student/student_dashboard.html', context)
---
+++ social_media/tests/views/student_views/test_student_views.py
@@ -0,0 +1,42 @@
+import self
+from django.test import TestCase, Client
+from django.urls import reverse
+from social_media.models import User
+from datetime import datetime
+
+from social_media.student_views import student_dashboard
+
+
+class StudentDashBoardViewTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            first_name= 'Jane',
+            last_name= 'Doe',
+            email = 'janedoe@email.com',
+            user_type= 'student',
+            university= 'kcl',
+            start_date= '23.09.2023',
+            end_date= '05.06.2026',
+
+        )
+
+    def test_student_dashboard_view(self):
+        # check student has successfully been logged in
+        login_success = self.client.login(username='janedoe@gmail.com', password='password123')
+        self.assertTrue(login_success)
+
+        # check that the student is able to access the dashboard
+        response = self.client.get(reverse('student_dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+        # check that it is the correct template
+        self.assertTemplateUsed(response, 'student/student_dashboard.html')
+
+        # check that the user accessing the page is a student
+        self.assertIn('user', response.context)
+        self.assertEqual(response.context['user'], 'student')
+
+
+
+
