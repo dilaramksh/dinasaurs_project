8e7d3471cbd42794c3da7427ba17eb09c2aa206f
Maryam Marei <maryam.marei@kcl.ac.uk>
2025-01-31 12:50:41 +0000

    change student_dashboard.html content to include content

--- social_media/student_views.py
+++ social_media/student_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from social_media.decorators import user_type_required
+from social_media.models import User
 
 
 #to do: add login required
@@ -9,11 +10,18 @@ from social_media.decorators import user_type_required
 #@user_type_required('student')
 #@login_required
 def student_dashboard(request):
+    #student_name = User.first_name + " " + User.last_name
+    #student_university = User.university
+    #student_email = User.email
+
+    #context = {
+     #   'student_name' : student_name,
+      #  'student_university' : student_university,
+       # 'student_email' : student_email
+    #}
+
     return render(request, 'student/student_dashboard.html')
 
-#to be added once login is configured
-#def student_dashboard(request):
- #   context = {
-  #      'user':'student',
-   # }
-    #return render(request, 'student/student_dashboard.html', context)
+   
+
+
