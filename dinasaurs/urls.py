10462e686dfe08f3a46abdca6c7cd94149933fab
Lara29527 <lara.ergenc@kcl.ac.uk>
2025-02-02 11:21:56 +0000

    Work in progress: Initial homepage implementation

--- dinasaurs/urls.py
+++ dinasaurs/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from social_media.student_views import student_dashboard
+from social_media.homepage_views import homepage
+#from django.conf import settings
+#from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', homepage, name='homepage'),
 
     #student paths
     path('student/dashboard/', student_dashboard, name='student_dashboard'),
