458e7ac4f11ef9b8e4625577830772f17723fdd3
Maryam Marei <maryam.marei@kcl.ac.uk>
2025-02-25 14:33:52 +0000

    add test for student dashboard

--- social_media/tests/views/student_views/test_student_views.py
+++ social_media/tests/views/student_views/test_student_views.py
@@ -1,43 +1,42 @@
-#import self
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from social_media.models import User, University
-from datetime import datetime
-
-from social_media.views.student_views import student_dashboard
-
 
 class StudentDashBoardViewTestCase(TestCase):
 
     def setUp(self):
-        university = University.objects.create(name='kcl')
+        university = University.objects.create(name="King's College London")
 
         self.user = User.objects.create(
-            first_name= 'Jane',
-            last_name= 'Doe',
-            email = 'janedoe@email.com',
-            user_type= 'student',
-            university= university,
-            start_date= '2023-09-23',
-            end_date= '2026-05-06',
-
+            first_name='jane',
+            last_name='doe',
+            email='janedoe@kcl.ac.uk',
+            user_type='student',
+            university=university,
+            start_date='2023-09-23',
+            end_date='2026-05-06',
+            username='@janedoe',
         )
+        self.user.set_password('Password123')
+        self.user.save()
+        self.login_url = reverse('log_in')
 
     def test_student_dashboard_view(self):
-        # check student has successfully been logged in
-        login_success = self.client.login(username='janedoe@gmail.com', password='password123')
+        login_success = self.client.login(username='@janedoe', password='Password123')
+        # check successful login
         self.assertTrue(login_success)
-
-        # check that the student is able to access the dashboard
-        response = self.client.get(reverse('student_dashboard'))
+        # check successful retrieval
+        response = self.client.get(reverse('dashboard'))
         self.assertEqual(response.status_code, 200)
-
-        # check that it is the correct template
+        # check correct template
         self.assertTemplateUsed(response, 'student/student_dashboard.html')
-
-        # check that the user accessing the page is a student
+        # check correct user type
         self.assertIn('user', response.context)
-        self.assertEqual(response.context['user'], 'student')
+        self.assertEqual(response.context['user'].user_type, 'student')
+
+
+
+
 
 
 
