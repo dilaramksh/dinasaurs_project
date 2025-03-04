d7bc4f11fa9572f0c688c4635152825900d3aaca
emilieyuu <yuemilie19@gmail.com>
2025-03-04 11:02:17 +0000

    Switching dashboards from profile menu full functionality

--- social_media/views/dashboard_views.py
+++ social_media/views/dashboard_views.py
@@ -1,6 +1,6 @@
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from social_media.models import Society, Membership, Event, SocietyRole
 from django.shortcuts import get_object_or_404
 
@@ -11,6 +11,10 @@ def dashboard(request):
     current_user = request.user
     user_type = current_user.user_type
 
+    active_society_id = request.session.get('active_society_id')
+    if active_society_id:
+        return redirect('society_dashboard', society_id=active_society_id)
+    
     context = {
         'user': current_user,
     }
@@ -42,8 +46,17 @@ def dashboard(request):
     return render(request, template, context)
 
 
-#@login_required
+@login_required
 def get_society_dashboard(request, society_id):
     """Display the dashboard for a specific society."""
     society = get_object_or_404(Society, pk=society_id)
+
+    # Store the selected society ID
+    request.session['active_society_id'] = society.id
     return render(request, 'society/society_dashboard.html', {'society': society})
+
+@login_required
+def get_student_dashboard(request):
+    """Clears the active society and redirects to the student dashboard."""
+    request.session.pop('active_society_id', None) 
+    return redirect('dashboard')
