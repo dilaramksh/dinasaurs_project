77fed6efaca04dcce96035ea5c22a735fa9c4dac
emilieyuu <yuemilie19@gmail.com>
2025-03-04 13:18:57 +0000

    toggle between society dashboard and main webpage functionality

--- social_media/views/society_page_view.py
+++ social_media/views/society_page_view.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from social_media.models import Society, Membership, Event
 
 
+#@login_required
 def society_mainpage(request, society_id):
     """Display the webpage for a specific society."""
     society = get_object_or_404(Society, pk=society_id)
@@ -13,6 +14,8 @@ def society_mainpage(request, society_id):
                          if membership.is_committee_member()]
     
     society_events = Event.objects.filter(society=society)
+
+    is_committee_member = request.user in committee_members
 
     society_colour1 = society.colour1
     society_colour2 = society.colour2
@@ -25,6 +28,7 @@ def society_mainpage(request, society_id):
         'society_events': society_events,
         'society_colour1': society_colour1,
         'society_colour2': society_colour2,
+        'is_committee_member': is_committee_member,
 
     }
 
