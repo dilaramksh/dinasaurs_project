baf4a14c9609168fd3258367f2c243c9b8b495af
Dina (Dina) Ghazaani <k23012496@kcl.ac.uk>
2025-03-07 19:33:20 +0000

    tests

---
+++ social_media/tests/views/society_views/test_creat_post_view 2.py
@@ -0,0 +1,73 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import resolve_url
+from social_media.models import Post
+from social_media.forms import PostForm
+'''
+class CreatePostViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.url = reverse('create_post')
+
+    def test_create_post_view_redirect_if_not_logged_in(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response, f'/accounts/login/?next={self.url}')
+
+    def test_create_post_view_post_valid_data(self):
+        self.client.login(username='testuser', password='password123')
+        post_data = {'title': 'Test Post', 'content': 'This is a test post.'}
+
+        response = self.client.post(self.url, post_data)
+
+        self.assertEqual(Post.objects.count(), 1)
+        post = Post.objects.first()
+        self.assertEqual(post.title, 'Test Post')
+        self.assertEqual(post.content, 'This is a test post.')
+        self.assertEqual(post.author, self.user)
+
+        self.assertContains(response, 'Post created successfully!')
+        self.assertRedirects(response, reverse('society_dashboard'))
+
+    def test_create_post_view_post_invalid_data(self):
+        self.client.login(username='testuser', password='password123')
+        post_data = {'title': '', 'content': ''}
+
+        response = self.client.post(self.url, post_data)
+
+        self.assertFormError(response, 'form', 'title', 'This field is required.')
+        self.assertFormError(response, 'form', 'content', 'This field is required.')
+
+        self.assertContains(response, 'Create a New Post')
+        self.assertContains(response, 'Submit')
+
+    def test_create_post_view_post_redirects_on_valid_data(self):
+        self.client.login(username='testuser', password='password123')
+        post_data = {'title': 'Test Post', 'content': 'This is a valid test post.'}
+
+        response = self.client.post(self.url, post_data)
+
+        self.assertRedirects(response, reverse('society_dashboard'))
+
+    def test_create_post_view_logged_in_user_can_see_their_posts(self):
+        self.client.login(username='testuser', password='password123')
+        post_data = {'title': 'Test Post', 'content': 'This is a test post.'}
+        self.client.post(self.url, post_data)
+
+        response = self.client.get(self.url)
+
+        self.assertContains(response, 'Your Posts')
+        self.assertContains(response, 'Test Post')
+        self.assertContains(response, 'This is a test post.')
+
+    def test_create_post_view_invalid_post_data(self):
+        self.client.login(username='testuser', password='password123')
+        post_data = {'title': '', 'content': ''}
+
+        response = self.client.post(self.url, post_data)
+
+        self.assertFormError(response, 'form', 'title', 'This field is required.')
+        self.assertFormError(response, 'form', 'content', 'This field is required.')
+'''
--- social_media/views/society_page_view.py
+++ social_media/views/society_page_view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from social_media.models import Society, Membership, Event
+from social_media.models.colour_history import SocietyColorHistory
 
 
 #@login_required
@@ -14,7 +15,7 @@ def society_mainpage(request, society_id):
                          if membership.is_committee_member()]
     
     society_events = Event.objects.filter(society=society)
-
+    past_colors = SocietyColorHistory.objects.filter(society=society).order_by('-updated_at')
     is_committee_member = request.user in committee_members
 
     society_colour1 = society.colour1
@@ -29,6 +30,7 @@ def society_mainpage(request, society_id):
         'society_colour1': society_colour1,
         'society_colour2': society_colour2,
         'is_committee_member': is_committee_member,
+        'past_colors': past_colors, 
 
     }
 
