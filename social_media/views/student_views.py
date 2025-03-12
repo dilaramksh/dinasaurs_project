13b65733656818275d670a0fbb26f30fc88a8aaf
emilieyuu <yuemilie19@gmail.com>
2025-03-12 08:51:33 +0000

    Dashboard management for super admin

--- social_media/views/student_views.py
+++ social_media/views/student_views.py
@@ -119,30 +119,36 @@ def view_societies(request):
 
 def student_societies(request):
     student = request.user
-
-    memberships = Membership.objects.filter(
-        user=student,
-        society_role__society__status="approved"
-    )
+    memberships = Membership.objects.filter(user=student)
     user_societies = [membership.society_role.society for membership in memberships]
     selected_society = None
 
     if request.method == 'GET' and 'society_id' in request.GET:
         society_id = request.GET['society_id']
-        selected_society = get_object_or_404(Society, id=society_id, status="approved")
+        selected_society = get_object_or_404(Society, id=society_id)
         if selected_society not in user_societies:
             selected_society = None
 
     if selected_society:
         society_roles = SocietyRole.objects.filter(society=selected_society)
+        committee_members = [
+            membership.user for membership in Membership.objects.filter(society_role__society=selected_society)
+            if membership.is_committee_member()
+        ]
     else:
         society_roles = SocietyRole.objects.filter(society__in=user_societies)
+
+        committee_members = [
+            membership.user for membership in Membership.objects.filter(society_role__society__in=user_societies)
+            if membership.is_committee_member()
+        ]
 
     return render(request, 'student/student_societies.html', {
         'student': student,
         'user_societies': user_societies,
         'selected_society': selected_society,
-        'society_roles': society_roles
+        'society_roles': society_roles,
+        'committee_members': committee_members,
     })
 
 def student_events(request):
