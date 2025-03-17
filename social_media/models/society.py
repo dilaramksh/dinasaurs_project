6a3b407d80b6fcb6f3dadfae91c6deeab7853aa4
Dilara Mukasheva (k23008889) <mukashevadilara41@gmail.com>
2025-03-17 22:23:13 +0000

    Update society.py

--- social_media/models/society.py
+++ social_media/models/society.py
@@ -33,7 +33,7 @@ class Society(models.Model):
             )
         ]
     )
-    logo = models.ImageField(upload_to='society_logos/', blank=False)
+    logo = models.ImageField(upload_to="society_logos/", blank=True, null=True, default=DEFAULT_SOCIETY_LOGO)
     termination_reason = models.CharField(max_length=50, choices=[('operational', 'Operational reasons'), ('low_interest', 'Low Interest'), ('financial', 'Financial reasons'), ('other', 'Other reason') ])
     status = models.CharField(max_length=20, choices=[("pending", "Pending"), ("approved", "Approved"), ("blocked", "Blocked")], default="pending")
     
