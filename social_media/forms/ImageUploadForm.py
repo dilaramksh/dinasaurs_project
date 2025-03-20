7e992772dba3be96a2da2e27661be3f71bcf9352
Dilara Mukasheva (k23008889) <mukashevadilara41@gmail.com>
2025-03-20 14:42:46 +0000

    events, post, news

--- social_media/forms/ImageUploadForm.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from social_media.models.profile_pictures import UploadedImage
-
-class ImageUploadForm(forms.ModelForm):
-    class Meta:
-        model = UploadedImage
-        fields = ["image"]
--- social_media/models/post.py
+++ social_media/models/post.py
@@ -4,12 +4,15 @@ from django import forms
 from django.core.exceptions import ValidationError
 from social_media.models.society import User, Society
 
+DEFAULT_PICTURE = "post_pictures/default.jpg"
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     society = models.ForeignKey(Society, on_delete=models.CASCADE, related_name="posts")
+    picture = models.ImageField(upload_to="post_pictures/", blank=True, null=True, default=DEFAULT_PICTURE)
 
     def __str__(self):
         return self.title
