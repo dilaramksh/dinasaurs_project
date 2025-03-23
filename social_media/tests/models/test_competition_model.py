dff6ccf158a00079fec1835845d66b261b1a8508
Lara Ergenc <lara.ergenc@kcl.ac.uk>
2025-03-23 16:53:23 +0300

    Test: verify string representation of competition participant displays username and competition name

--- social_media/tests/models/test_competition_model.py
+++ social_media/tests/models/test_competition_model.py
@@ -1,16 +1,20 @@
 from django.test import TestCase
-from social_media.models import Competition, Society, User, University, Category
+from social_media.models import Competition, Society, User, University, Category, CompetitionParticipant
 from django.utils.timezone import now, timedelta
 from django.utils import timezone
 
 class CompetitionModelTestCase(TestCase):
     def setUp(self):
+        self.university, _ = University.objects.get_or_create(
+            name='Test Uni',
+            domain='testuni.ac.uk'
+        )
         self.society = Society.objects.create(
             name="Test Society",
             founder=User.objects.create_user(
                 username='@founder', email='founder@test.com', password='12345',
                 first_name='Test', last_name='Founder', user_type='student',
-                university=University.objects.create(name='Test Uni', domain='testuni.ac.uk'),
+                university=self.university,
                 start_date=now().date(), end_date=(now() + timedelta(days=365)).date()
             ),
             society_email='test@soc.com',
@@ -33,9 +37,21 @@ class CompetitionModelTestCase(TestCase):
             is_finalized=False
         )
 
+        self.user1 = User.objects.create_user(
+            username='@user1', email='user1@test.com', password='12345',
+            first_name='User', last_name='One', user_type='student',
+            university=self.university,
+            start_date=now().date(), end_date=(now() + timedelta(days=365)).date()
+        )
+
+        self.participant1 = CompetitionParticipant.objects.create(user=self.user1, competition=self.competition)
+
     def test_competition_creation(self):
         self.assertEqual(self.competition.name, "Spring Coding Challenge")
         self.assertTrue(self.competition.is_point_based)
         self.assertFalse(self.competition.is_finalized)
         self.assertEqual(str(self.competition), "Test Society: Spring Coding Challenge")
+
+    def test_competition_participant_str(self):
+        self.assertEqual(str(self.participant1), "@user1 in Spring Coding Challenge")
 
