6ab96d0b177735f3c00c1184567fb957260ee0ca
emilieyuu <yuemilie19@gmail.com>
2025-03-23 10:27:34 +0000

    Removed unused decorator

--- social_media/helpers.py
+++ social_media/helpers.py
@@ -25,17 +25,6 @@ def membership_required(view_function):
         return view_function(request, society_id, *args, **kwargs)
     return modified_view_function
 
-def user_type_required(user_type):
-    def decorator(view_function):
-        @wraps(view_function)
-        def _wrapped_view(request, *view_args, **view_kwargs):
-            print("User: ", request.user, request.user.user_type)
-            if getattr(request.user, 'user_type', None) == user_type:
-                return view_function(request, *view_args, **view_kwargs)
-            return render(request, 'unauthorized_page.html')
-        return _wrapped_view
-    return decorator
-
 """
 Helpers
 """
