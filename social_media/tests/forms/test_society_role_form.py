3fcb6dd0150b1b6a025bf49bb2a091f6ac416b8f
Lara Ergenc <lara.ergenc@kcl.ac.uk>
2025-03-24 00:24:49 +0300

    creating setup

--- social_media/tests/forms/test_society_role_form.py
+++ social_media/tests/forms/test_society_role_form.py
@@ -1,7 +1,22 @@
 from django.test import TestCase
 from social_media.forms.society_role_form import SocietyRoleForm
+from django.utils.timezone import now, timedelta
+from social_media.models import User, University
 
 class SocietyRoleFormTests(TestCase):
+    def setUp(self):
+        self.university = University.objects.create(name="Test Uni", domain="testuni1.ac.uk")
+        self.user = User.objects.create_user(
+            username='@tester',
+            email='tester@example.com',
+            password='testpass',
+            first_name='Test',
+            last_name='User',
+            user_type='student',
+            university=self.university,
+            start_date=now().date(),
+            end_date=(now() + timedelta(days=365)).date()
+        )
     def test_valid_role_name(self):
         """Test that SocietyRoleForm accepts a valid role name."""
         form_data = {'role_name': 'Treasurer'}
